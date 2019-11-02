1fbb910313261cc588f7efda141e4ee7a02b44ac
ryleybridges <rye.brid@gmail.com>
2019-11-02 16:14:09 +1300

    Added features

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="container" id="mainBody">
     <?php if (have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
-            
+
         <?php endwhile; ?>
     <?php endif; ?>
 </div>
